565af9de9a200d969d455a64182e33473a4c17d5
EddyArellanes <you@example.com>
2018-10-25 18:29:32 -0500

    First

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello World in Nuxt</h1>
+</template>
+<script>
+import axios from 'axios'
+import env from '../config/env'
+export default {
+  name: 'IndexPage',
+  created(){
+    axios.get(`${env.endpoint}/albums`).then(data=>{
+      console.log(data)
+    }).catch(err=>{
+
+    }).finally(()=>{
+
+    })
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
+
